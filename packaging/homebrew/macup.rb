a5d32b58d3acd7b6cc31377557f053efff557120
Dickson K <dickson809@gmail.com>
2026-08-04 23:58:34 +1000

    chore: fill in release tarball sha256 for v0.1.0 formula

--- packaging/homebrew/macup.rb
+++ packaging/homebrew/macup.rb
@@ -2,7 +2,7 @@ class Macup < Formula
   desc "Bootstrap and keep a Mac dev environment in sync"
   homepage "https://github.com/dicksonk/macup"
   url "https://github.com/dicksonk/macup/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_TARBALL_SHA256"
+  sha256 "a69ae06f6eb95cc6fa549421fcb22c9a05b35b7ff4f4cbba1116aa577a082c21"
   license "MIT"
 
   depends_on "git"
